88a662cc108e3e46544c58b18ecc3d2496319860
willbyrne <will.bl.byrne@gmail.com>
2022-10-19 17:17:49 +0100

    complete ticket 2c

--- app.js
+++ app.js
@@ -16,27 +16,36 @@ app.get("/", function (req, res) {
   res.send("Welcome to cwissy.rest");
 });
 
+
 app.get("/api/quotes", async function (req, res) {
+  //call all the quotes
   const arrayQuotes = await getQuotes();
-  res.send(arrayQuotes);
+  //call random quote
+  let randomQuote = await getRandomQuote()
+  // save query object
+  let queryType = req.query.type
+  //  check if query type matches random
+  if (queryType == 'random'){
+    // respond with randomQuote
+    res.json(randomQuote);
+    // if not respond with all the quotes
+  } else {
+    res.send(arrayQuotes);
+  }
 });
 
-app.get("/api/quotes?type=random", function (req, res) {
-  // use getRandomQuote
-  // save in variable
-   req.query
-  // if (randomQuery === 'random'){
-    res.json(req.query)
-  
-  // const randomQuoteObject = getRandomQuote();
-  // console.log(randomQuoteObject);
-  // console.log(getRandomQuote())
-  // res.json(randomQuoteObject);
-});
+//create post request handler to api/quotes
+// use addQuote using body of request as parameter
+//req.body.quoteText
+// respond with new quote object
 
-// get route handler
-// use getQuotes
-
+app.post("/api/quotes", async function (req, res){
+  const newQuoteText = req.body.quoteText
+  console.log(newQuoteText);
+  const newQuote = await addQuote(newQuoteText);
+  res.json(newQuote);
+}
+)
 
 app.listen(port, function () {
   console.log(`Server listening on port ${port}`);
